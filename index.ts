cb5cff8a318b1dda3343563e3a464500053c4ba4
MD Ibna Abid <mdabout2@gmail.com>
2026-07-19 21:13:08 +0600

    add cahnges

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://foodiezone-nu.vercel.app",
     credentials: true,
   })
 );
